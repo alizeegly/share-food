80ab6f8c857522d52926c76157bf14c326d2c21a
BUKAL JOHANA <johana.bukal@etu.unistra.fr>
2023-07-16 16:32:47 +0200

    Ajouter photo

--- lib/screens/profile/products/create_product_screen.dart
+++ lib/screens/profile/products/create_product_screen.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:sharefood/controllers/profile_controller.dart';
 import 'package:sharefood/models/user_model.dart';
@@ -9,6 +11,7 @@ import 'package:sharefood/widgets/custom_button.dart';
 import 'package:sharefood/widgets/form_fields/custom_date_time_field.dart';
 import 'package:sharefood/widgets/form_fields/custom_number_field.dart';
 import 'package:sharefood/widgets/form_fields/custom_text_field.dart';
+import 'package:firebase_storage/firebase_storage.dart' as fStorage;
 
 class CreateProductScreen extends StatefulWidget {
   const CreateProductScreen({super.key});
@@ -23,7 +26,28 @@ class _CreateProductScreenState extends State<CreateProductScreen> {
   TextEditingController priceController = TextEditingController(text: "");
   TextEditingController typeController = TextEditingController(text: "");
 
-  void saveProduct() async {
+  String? _setDate;
+  late String dateTime;
+  DateTime selectedDate = DateTime.now();
+  final TextEditingController _dateController = TextEditingController();
+
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+  String pictureUrl = "";
+  bool isLoading = false;
+
+  Future<void> saveProduct() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    // Uploader l'image
+    String filename = DateTime.now().millisecondsSinceEpoch.toString();
+    fStorage.Reference reference = fStorage.FirebaseStorage.instance.ref().child("products").child(filename);
+    fStorage.UploadTask uploadTask = reference.putFile(File(imageXFile!.path)); // TODO VALIDATION
+    fStorage.TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => {});
+    pictureUrl = await taskSnapshot.ref.getDownloadURL();
+
     // Créer l'objet json
     final controller = Get.put(ProfileController());
     UserModel user = await controller.getUserData();
@@ -32,23 +56,20 @@ class _CreateProductScreenState extends State<CreateProductScreen> {
       "expirationDate": Timestamp.fromDate(DateTime(selectedDate.year, selectedDate.month, selectedDate.day, 0, 0)),
       "name": nameController.text.trim(),
       "order": false,
-      "pictureUrl": "https://www.bricozor.com/static/img/visuel-indisponible-650.png",
+      "pictureUrl": pictureUrl,
       "price": double.parse(priceController.text.trim().replaceAll(',', '.')),
       "seller": FirebaseFirestore.instance.doc("sellers/${user.id}"),
       "type": typeController.text.trim()
     };
 
     // Créer le produit dans Firestore
-    /*DocumentReference<Map<String, dynamic>> newProductsnapshot =*/ await FirebaseFirestore.instance.collection("products").add(product);
+    await FirebaseFirestore.instance.collection("products").add(product);
+
+    setState(() {
+      isLoading = false;
+    });
   }
 
-  // Datetime picker
-  String? _setDate;
-  late String dateTime;
-  DateTime selectedDate = DateTime.now();
-
-  final TextEditingController _dateController = TextEditingController();
-  
   Future<void> _selectDate(BuildContext context) async {
     final DateTime? picked = await showDatePicker(
       context: context,
@@ -64,6 +85,13 @@ class _CreateProductScreenState extends State<CreateProductScreen> {
         _dateController.text = DateFormat("dd/MM/yyyy").format(selectedDate);
       });
     }
+  }
+
+  Future<void> _getImage() async {
+    imageXFile = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      imageXFile;
+    });
   }
 
   @override
@@ -85,7 +113,35 @@ class _CreateProductScreenState extends State<CreateProductScreen> {
             children: [
               Form(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
+                    const Text("Ajouter une photo", textAlign: TextAlign.left),
+                    const SizedBox(height: 8),
+                    InkWell(
+                      onTap: () {
+                        _getImage();
+                      },
+                      child: Container(
+                        width: 200,
+                        height: 200,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30), // Optional: Add border radius for rounded corners
+                          image: imageXFile == null ? null : DecorationImage(
+                            image: FileImage(File(imageXFile!.path)),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        child: imageXFile == null ? Icon(
+                          Icons.add_a_photo,
+                          size: MediaQuery.of(context).size.width * 0.09,
+                          color: Colors.grey,
+                        ) : null,
+                      ),
+                    ),
+
+                    const SizedBox(height: 17),
+
                     CustomTextField(
                       controller: nameController,
                       hintText: "Nom",
@@ -134,12 +190,12 @@ class _CreateProductScreenState extends State<CreateProductScreen> {
               ),
               const SizedBox(height: 30),
               CustomButton(
-                onPressed: () {
-                  saveProduct();
+                onPressed: () async {
+                  await saveProduct();
                   Navigator.pop(context);
                 },
                 color: colors.primary,
-                text: "Proposer mon produit",
+                text: isLoading ? "Patientez..." : "Proposer mon produit",
               )
             ],
           ),
